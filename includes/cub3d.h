bd5303c7e5a9d6e916eea9ce9f998a090a82a794
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-03-01 18:09:20 +0100

    update

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 15:16:49 by ltomasze          #+#    #+#             */
-/*   Updated: 2025/02/23 14:24:15 by ltomasze         ###   ########.fr       */
+/*   Updated: 2025/03/01 13:52:41 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
-# include "../libft/libft.h"
 # include "../includes/get_next_line.h"
+
+typedef struct s_game
+{
+    void    *mlx;
+    void    *win;
+	char    **map;
+}   t_game;
+
+
+#define ESC_KEY 65307  // ESC
