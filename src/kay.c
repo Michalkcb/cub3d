5855245c566331c16bc33e6e9170780b4f192661
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-03-04 20:04:12 +0100

    add rotatye handling

--- src/kay.c
+++ src/kay.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:21:28 by mbany             #+#    #+#             */
-/*   Updated: 2025/03/04 19:12:03 by mbany            ###   ########.fr       */
+/*   Updated: 2025/03/04 19:57:28 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,31 @@
 
 int handle_key(int key, t_game *game)
 {
+    int new_x = game->player_x;
+    int new_y = game->player_y;
+
     if (key == ESC_KEY)
         clean_exit(game);
     else if (key == W_KEY)
-        game->player_y -= game->move_speed;
+        new_y -= game->move_speed;
     else if (key == S_KEY)
-        game->player_y += game->move_speed;
+        new_y += game->move_speed;
     else if (key == A_KEY)
-        game->player_x -= game->move_speed;
+        new_x -= game->move_speed;
     else if (key == D_KEY)
-        game->player_x += game->move_speed;
+        new_x += game->move_speed;
+    else if (key == LEFT_ARROW_KEY)
+        game->player_dir = (game->player_dir - game->rotate_speed) % 360;
+    else if (key == RIGHT_ARROW_KEY)
+        game->player_dir = (game->player_dir + game->rotate_speed) % 360;
 
-    draw_map(game); // Redraw the map with the updated player position
+    // Check for collision with walls
+    if (game->map[new_y][new_x] != '1')
+    {
+        game->player_x = new_x;
+        game->player_y = new_y;
+    }
+
+    draw_map(game); // Redraw the map with the updated player position and direction
     return (0);
 }
