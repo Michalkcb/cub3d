fa310f3c29c63205adc5369b486ecd9179b41e86
Michael <126069497+Michalkcb@users.noreply.github.com>
2025-04-06 16:54:59 +0200

    norminette update

--- src/init_game.c
+++ src/init_game.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 15:11:49 by mbany             #+#    #+#             */
-/*   Updated: 2025/04/05 19:52:58 by mbany            ###   ########.fr       */
+/*   Updated: 2025/04/06 16:51:46 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,51 @@ void	init_ray(t_ray *ray, t_player *player, float ray_angle)
 	ray->mapx = (int)(player->x / BLOCK);
 	ray->mapy = (int)(player->y / BLOCK);
 }
+
+int	set_player_angle_and_position(t_game *game, int x, int y, char direction)
+{
+	game->player.x = x * BLOCK + BLOCK / 2;
+	game->player.y = y * BLOCK + BLOCK / 2;
+	if (direction == 'N')
+		game->player.angle = 3 * PI / 2;
+	else if (direction == 'S')
+		game->player.angle = PI / 2;
+	else if (direction == 'E')
+		game->player.angle = 0;
+	else if (direction == 'W')
+		game->player.angle = PI;
+	else
+		return (0);
+	game->map[y][x] = '0';
+	return (1);
+}
+
+void	set_player_position(t_game *game)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (game->map[y])
+	{
+		x = 0;
+		while (game->map[y][x])
+		{
+			if (game->map[y][x] == 'N' || game->map[y][x] == 'S' ||
+				game->map[y][x] == 'E' || game->map[y][x] == 'W')
+			{
+				if (set_player_angle_and_position(game, x, y, game->map[y][x]))
+					return ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
 void	init_player(t_player *player)
 {
-	player->x = 587;
-	player->y = 166;
-	player->angle = PI / 1.8;
+	player->angle = PI;
 	player->key_up = false;
 	player->key_down = false;
 	player->key_left = false;
@@ -32,7 +72,7 @@ void	init_player(t_player *player)
 	player->rotate_right = false;
 }
 
-t_config	config;
+// t_config	config;
 void	init_game_struct(t_game *game)
 {
 	game->mlx = NULL;
@@ -42,17 +82,16 @@ void	init_game_struct(t_game *game)
 	game->map = game->config.map;
 	game->copy_map = NULL;
 	game->map = game->config.map;
-		
+	game->color_floor = game->config.floor_color;
+	game->color_ceiling = game->config.ceiling_color;
 	game->n_tex_path = game->config.textures[0];
-    game->s_tex_path = game->config.textures[1];
-    game->w_tex_path = game->config.textures[2];
-    game->e_tex_path = game->config.textures[3];
-
+	game->s_tex_path = game->config.textures[1];
+	game->w_tex_path = game->config.textures[2];
+	game->e_tex_path = game->config.textures[3];
 	game->n_path = 0;
-    game->s_path = 0;
-    game->w_path = 0;
-    game->e_path = 0;
-
+	game->s_path = 0;
+	game->w_path = 0;
+	game->e_path = 0;
 }
 
 int	init_game(t_game *game, char *file)
@@ -60,12 +99,7 @@ int	init_game(t_game *game, char *file)
 	init_game_struct(game);
 	init_player(&game->player);
 	game->fd = open(file, O_RDONLY);
-	/////////////////////////////////////////
-	// if (!parsing(game))
-	// 	return (error(INVALID_MAP, game));
-
-	
-	/////////////////////////////////////////
+	set_player_position(game);
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "cub3d");
 	game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
@@ -74,149 +108,6 @@ int	init_game(t_game *game, char *file)
 	if (!load_all_textures(game))
 		return (error(INVALID_FILE, game));
 	mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+	mlx_mouse_move(game->mlx, game->win, WIDTH / 2, HEIGHT / 2);
 	return (1);
 }
-
-
-//tmp===================================================
-
-
-
-/*
-int	parsing(t_game *data)
-{
-    char	*line;
-    int		is_map_started;
-
-    is_map_started = 0;
-    while (1)
-    {
-        line = get_next_line(data->fd);
-        if (!line)
-            break ;
-        if (!is_map_started)
-        {
-            if (!line_redirection(line, &is_map_started))
-                return (0);
-        }
-        if (is_map_started)
-        {
-            create_map(line, data);
-		
-            break ;
-        }
-        if (line)
-            free(line);
-    }
-    return (1);
-}
-
-static int	is_map_line(char *line)
-{
-	int	i;
-
-	i = 0;
-	while (line[i] == ' ')
-		i++;
-	if (!line[i] || line[i] == '\n')
-		return (0);
-	while (line[i] && line[i] != '\n')
-	{
-		if (!ft_strchr("01NSEW ", line[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	line_redirection(char *line, int *is_map_started)
-{
-	if (!ft_strcmp(line, "\n"))
-	{
-		line = NULL;
-		free(line);
-		return (1);
-	}
-	if (!line_check(line))
-	{
-		free(line);
-		return (0);
-	}
-	if (is_map_line(line))
-		*is_map_started = 1;
-	return (1);
-}
-
-
-void	free_split(char **arr)
-{
-	int	i;
-
-	if (!arr)
-		return ;
-	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
-		i++;
-	}
-	free(arr);
-}
-
-
-
-int	line_check(char *line)
-{
-    if (!ft_strcmp(line, "\n"))
-        return (0);
-    else
-        return (1);
-}
-
-int	create_map(char *line, t_game *data)
-{
-	char	*next;
-
-	while (1)
-	{
-		next = get_next_line(data->fd);
-		if (!next)
-			break ;
-		line = ft_strjoin_gnl(line, next);
-		free(next);
-	}
-	data->map = ft_split(line, '\n');
-	data->copy_map = ft_split(line, '\n');
-	close(data->fd);
-	free(line);
-	return (1);
-}
-
-char	*ft_strjoin_gnl(char *buffer, char *new_s)
-{
-	int		i;
-	int		j;
-	char	*string;
-	int		length;
-
-	length = 0;
-	i = 0;
-	j = 0;
-	if (!buffer || !new_s)
-		return (NULL);
-	length = ft_strlen(buffer) + ft_strlen(new_s);
-	string = (char *)malloc((sizeof(char) * (length + 1)));
-	if (!string)
-		return (NULL);
-	while (buffer[i] != '\0')
-		string[j++] = buffer[i++];
-	i = 0;
-	while (new_s[i] != '\0')
-		string[j++] = new_s[i++];
-	string[j] = '\0';
-	if (buffer)
-		free(buffer);
-	buffer = NULL;
-	return (string);
-}
-	*/
